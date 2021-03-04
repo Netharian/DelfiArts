3a159da4b1d55b4c6aa438932e786991d7bd7309
Lucas Melo <lucasmfs21@gmail.com>
2021-03-04 20:45:55 -0300

    commission link adjusted

--- src/components/Commission.tsx
+++ src/components/Commission.tsx
@@ -3,7 +3,8 @@ import styles from '../styles/pages/Commission.module.css';
 export function Commission() {
     return (
         <div className={styles.commissionContainer}>
-            <a href="/commissionme"><img src="dupla.jpg" alt="duplinha dinamica" />
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdVxzYCRSvGRxW2yV4VN583MWfyXS6cPZjArblzJS3j8LHDIg/viewform" target="_blank" rel="noopener noreferrer">
+                <img src="dupla.jpg" alt="duplinha dinamica" />
                 <span> COMMISSION ME </span></a>
         </div>
     );
